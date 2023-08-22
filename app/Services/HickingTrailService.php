ba83013d2e0b93f9cf0f54282660bf6217009620
ppitacaneiro@gmail.com <ppitacaneiro@gmail.com>
2023-08-22 20:23:44 +0200

    create trail method

--- app/Services/HickingTrailService.php
+++ app/Services/HickingTrailService.php
@@ -1,6 +1,11 @@
 <?
 namespace App\Services;
+use App\Models\HickingTrail;
 
 class HickingTrailService 
 {
+    public function create(array $data) : HickingTrail
+    {
+        return HickingTrail::create($data);
+    }
 }
